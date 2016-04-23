26e89d96541e4a5f10ff22bcdbdd2f64937177f7
various.authors <various.authors@gmail.com>
2016-04-23 15:54:22 -0700

    make and draw empty board

--- love/draw.lua
+++ love/draw.lua
@@ -1,5 +1,33 @@
 love.viewport = require('libs/viewport').newSingleton()
 
 function love.draw()
-    -- Draw here
+    love.graphics.push()
+    love.graphics.scale(game.scale, game.scale)
+
+    local cells = game.board.cells
+    local w = game.constants.cell_dim
+    local h = game.constants.cell_dim
+    local gutter = game.constants.cell_gutter
+
+    for y = 1, #(cells), 1 do
+
+        for x = 1, #(cells[y]), 1 do
+            local cell = cells[y][x]
+            local x = (x * gutter) + ((x - 1) * w)
+            local y = (y * gutter) + ((y - 1) * h)
+
+            if cell == EMPTY then
+
+                love.graphics.setColor({ 50, 50, 50 })
+                love.graphics.rectangle("fill", x + w/2 - 0.5, y + h/2 - 0.5, 1, 1)
+                love.graphics.setColor({ 255, 255, 255 })
+            else
+
+                love.graphics.rectangle("fill", x, y, w, h)
+            end
+        end
+    end
+
+    love.graphics.scale(1)
+    love.graphics.pop()
 end
--- love/load.lua
+++ love/load.lua
@@ -1,4 +1,29 @@
+EMPTY = 0
+
 function love.load()
     require('game/controls')
     require('game/sounds')
+
+    game = {
+        scale = 2
+    }
+
+    game.constants = {
+        height = 18,
+        width = 7,
+        cell_dim = 5,
+        cell_gutter = 1
+    }
+
+    game.board = {
+        cells = {}
+    }
+
+    for y = 1, game.constants.height, 1 do
+        game.board.cells[y] = {}
+
+        for x = 1, game.constants.width, 1 do
+            game.board.cells[y][x] = EMPTY
+        end
+    end
 end
